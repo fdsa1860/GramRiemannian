8ec9211bed03963e880b94988799e483b692331a
xikang <zhangxk2009@gmail.com>
2015-10-19 14:07:58 -0400

    minor clean

--- matlab/clustering/kmeansJLD.m
+++ matlab/clustering/kmeansJLD.m
@@ -8,7 +8,6 @@ function [label,X_center,D] = kmeansJLD(X,k,opt)
 % label: the clustered labeling results
 
 N = length(X);
-% D = zeros(k,N);
 ind = randsample(N,k);
 X_center = X(ind);
 label = ones(1,N);
@@ -17,19 +16,7 @@ label_old = zeros(1,N);
 iter = 0;
 iter_max = 100;
 labelChangeThreshold = N * 0.01;
-while iter<iter_max && nnz(label-label_old)>labelChangeThreshold
-    
-%     for i=1:k
-%         for j=1:N
-%             if strcmp(opt.metric,'JLD')
-%                 HH1 = X_center{i};
-%                 HH2 = X{j};
-%                 D(i,j) = log(det((HH1+HH2)/2)) - 0.5*log(det(HH1)) - 0.5*log(det(HH2));
-%             elseif strcmp(opt.metric,'binlong')
-%                 D(i,j) = 2 - norm(X{i}+X_center{j},'fro');
-%             end
-%         end
-%     end
+while iter<iter_max && nnz(label-label_old)>labelChangeThreshold   
     D = HHdist(X_center, X, opt);
     label_old = label;
     [~,label] = min(D);
@@ -63,15 +50,6 @@ end
 
 function center = findCenter(X,opt)
 
-% n = length(X);
-% D = zeros(n,n);
-% for i=1:n
-%     for j=i+1:n
-% %         D(i,j) = hankeletAngle(X{i},X{j},thr);
-%         D(i,j) = 2 - norm(X{i}+X{j},'fro');
-%     end
-% end
-% D = D + D';
 D = HHdist(X,[],opt);
 d = sum(D);
 [~,ind] = min(d);
